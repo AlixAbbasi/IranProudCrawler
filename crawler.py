86deeaf32b1a0fdc2d81e28ae36c2a46c552bbd6
bl4ckic3 <bl4ckic3@users.noreply.github.com>
2015-09-28 10:53:21 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -21,7 +21,7 @@ BaseURL="http://www.iranproud.com"
 #url2="http://www.iranproud.com"
 req = urllib2.Request(url)
 print "Connecting to IranProud server"
-req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2015092717 Firefox/3.0.3')
 response = urllib2.urlopen(req)
 link=response.read()
 response.close()
