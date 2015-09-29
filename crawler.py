644349b939d07bfcb65aa03e49a884efeba65c99
Ali <bl4ckic3@users.noreply.github.com>
2015-09-29 14:28:02 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -14,11 +14,9 @@ class bcolors:
    UNDERLINE = '\033[4m'
 m3utext="#EXTINF:-1,"
 logohandler=" #IMG:"
-#logoemitter=""
 print bcolors.BOLD + "System Initialization to capture live tv channels from IranProud server" + bcolors.BOLD
 url='http://www.iranproud.com/livetv'
 BaseURL="http://www.iranproud.com"
-#url2="http://www.iranproud.com"
 req = urllib2.Request(url)
 print "Connecting to IranProud server"
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2015092817 Firefox/3.0.3')
@@ -31,7 +29,6 @@ i=len(match)-1
 x=0
 print ("Detected %i Live TV channel in IranProud server..." % (i)) 
 while i>=x:
-    #print x
     for subdir,logo in match:
         mainurl=BaseURL+subdir
         req2 = urllib2.Request(mainurl)
@@ -46,14 +43,12 @@ while i>=x:
              print bcolors.OKBLUE + ChannelNameFinal + bcolors.OKBLUE
              print logo
              with open("tvlist.m3u", "ab") as myfile:
-                 #myfile.write(m3utext+ChannelNameFinal+logohandler+logo)
                  myfile.write(m3utext+ChannelNameFinal)
                  myfile.write("\n")
                  myfile.write(ChannelURL)
                  myfile.write("\n")
                  myfile.write("\n")
                  myfile.close
-             #print logo
         match2 = None
         link2 = None
         response2 = None
@@ -61,29 +56,4 @@ while i>=x:
         mainurl = None
         print x
         x = x+1
-#print match
-#pmc = match[10][0] 
-#len(match)
-#mainurl=url2+pmc
 
-#f = open('test.txt','r')
-#filedata = f.read()
-#f.close()
-#newdata = filedata.replace("@","\n")
-#f = open('test2.txt','w')
-#f.write(newdata)
-#f.close()
-'''
-print mainurl
-req2 = urllib2.Request(mainurl)
-req2.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-response2 = urllib2.urlopen(req2)
-link2=response2.read()
-response2.close()
-match2=re.compile('.+? videosrc="(.+?)".+?').findall(link2)
-print match2
-'''
-#filemesscleaner()
-#print response.info()
-#link
-#match = re.findall(r'<p>(.*?)</p>',str(respData))
